180af4db4a80a47a2a955c2eb73eccacc723a529
VladG <gricenkovlad007@gmail.com>
2025-02-27 12:13:36 +0300

    Solved 26_2652

--- 26/2652.py
+++ 26/2652.py
@@ -3,6 +3,6 @@ data = [int(x) for x in open("26_2652.txt")]
 
 
 res = [(x, data.count(x)) for x in data]
-res.sort(key=lambda x: x[1], reverse=True)
+res.sort(key=lambda x: x[1])
 print(res, len(set(data)))
 
